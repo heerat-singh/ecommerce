11b9e1c2fa33f31d27378328b624cfaf8c695ab4
heerat-singh <heeratsingh54@gmail.com>
2023-10-28 15:01:25 -0400

    Product Entity Added

---
+++ src/main/java/com/heeratsingh/ecommerce/model/Product.java
@@ -0,0 +1,131 @@
+package com.heeratsingh.ecommerce.model;
+
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Objects;
+import java.util.Set;
+
+import jakarta.persistence.CascadeType;
+import jakarta.persistence.Column;
+import jakarta.persistence.ElementCollection;
+import jakarta.persistence.Embedded;
+import jakarta.persistence.Entity;
+
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.OneToMany;
+import lombok.Data;
+
+@Entity
+@Data
+public class Product {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+
+    @Column(name = "title")
+    private String title;
+
+    @Column(name = "description")
+    private String description;
+
+    @Column(name = "price")
+    private int price;
+
+    @Column(name = "discounted_price")
+    private int discountedPrice;
+
+    @Column(name="discount_persent")
+    private int discountPersent;
+
+    @Column(name = "quantity")
+    private int quantity;
+
+    @Column(name = "brand")
+    private String brand;
+
+    @Column(name = "color")
+    private String color;
+
+    @Embedded
+    @ElementCollection
+    @Column(name = "sizes")
+    private Set<Size> sizes=new HashSet<>();
+
+    @Column(name = "image_url")
+    private String imageUrl;
+
+    @OneToMany(mappedBy = "product",cascade = CascadeType.ALL,orphanRemoval = true)
+    private List<Rating>ratings=new ArrayList<>();
+
+    @OneToMany(mappedBy = "product",cascade = CascadeType.ALL,orphanRemoval = true)
+    private List<Review>reviews=new ArrayList<>();
+
+    @Column(name = "num_ratings")
+    private int numRatings;
+
+
+    @ManyToOne()
+    @JoinColumn(name="category_id")
+    private Category category;
+
+    private LocalDateTime createdAt;
+
+    public Product() {
+
+    }
+
+    public Product(Long id, String title, String description, int price, int discountedPrice, int discountPersent,
+                   int quantity, String brand, String color, Set<Size> sizes, String imageUrl, List<Rating> ratings,
+                   List<Review> reviews, int numRatings, Category category, LocalDateTime createdAt) {
+        super();
+        this.id = id;
+        this.title = title;
+        this.description = description;
+        this.price = price;
+        this.discountedPrice = discountedPrice;
+        this.discountPersent = discountPersent;
+        this.quantity = quantity;
+        this.brand = brand;
+        this.color = color;
+        this.sizes = sizes;
+        this.imageUrl = imageUrl;
+        this.ratings = ratings;
+        this.reviews = reviews;
+        this.numRatings = numRatings;
+        this.category = category;
+        this.createdAt = createdAt;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(brand, category, color, description, discountPersent, discountedPrice, id, imageUrl,
+                numRatings, price, quantity, ratings, reviews, sizes, title);
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj)
+            return true;
+        if (obj == null)
+            return false;
+        if (getClass() != obj.getClass())
+            return false;
+        Product other = (Product) obj;
+        return Objects.equals(brand, other.brand) && Objects.equals(category, other.category)
+                && Objects.equals(color, other.color) && Objects.equals(description, other.description)
+                && discountPersent == other.discountPersent && discountedPrice == other.discountedPrice
+                && Objects.equals(id, other.id) && Objects.equals(imageUrl, other.imageUrl)
+                && numRatings == other.numRatings && price == other.price && quantity == other.quantity
+                && Objects.equals(ratings, other.ratings) && Objects.equals(reviews, other.reviews)
+                && Objects.equals(sizes, other.sizes) && Objects.equals(title, other.title);
+    }
+
+}
+
